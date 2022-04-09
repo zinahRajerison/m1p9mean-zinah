128fc4dba1a96e6d1b6ec2ec565dd6dee844b909
Zinah <rzinahrajerison2000@gmail.com>
2022-04-09 16:30:29 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -212,6 +212,6 @@ app.get('/findAllCommande',function(req,res){
         res.send(toRespond)
     })
 });
-app.listen(3000,function(){
+app.listen(process.env.PORT || 5000,function(){
     console.log('Example app listening on port 3000')
 });
