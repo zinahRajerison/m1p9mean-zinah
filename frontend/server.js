52d3a9e94cbc0880a0abe360eb5f8f539fba86f6
Zinah <rzinahrajerison2000@gmail.com>
2022-04-09 20:24:29 +0300

    update

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,7 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '/index'));
-});
-
-app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
-app.listen(process.env.PORT || 3000);
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/front'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/front/index.html'));});
+app.listen(process.env.PORT || 8080);
