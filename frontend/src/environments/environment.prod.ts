e2cc886c48ea8b41b71b928d162cab509b341b35
Zinah <rzinahrajerison2000@gmail.com>
2022-04-09 21:14:34 +0300

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true
 };
-export const base_url = "https://m1p9meanfront-zinah.herokuapp.com/";
+export const base_url = "https://m1p9meanfront-zinah.herokuapp.com";
 
 export const user = {
   token : ''
