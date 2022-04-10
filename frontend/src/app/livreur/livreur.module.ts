101e34db05c802bb194797738c7e2f26e029fd73
Zinah <rzinahrajerison2000@gmail.com>
2022-04-10 20:35:57 +0300

    Update livreur.module.ts

--- frontend/src/app/livreur/livreur.module.ts
+++ frontend/src/app/livreur/livreur.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LivreurComponent } from '../livreur/livreur.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { CommandeComponent } from './commande/commande.component';
 import { LivraisonComponent } from './livraison/livraison.component';
 
 
 
 @NgModule({
-  declarations: [LivreurComponent, NavbarComponent, CommandeComponent, LivraisonComponent],
+  declarations: [LivreurComponent, NavbarComponent, LivraisonComponent],
   imports: [
     CommonModule
   ]
